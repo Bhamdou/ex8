e81e6ea35c0e91afe7535918ba4a4837c52dd591
Bhamdou <qgubb21@gmail.com>
2022-08-26 10:29:53 +0200

    first commit

---
+++ ex8.py
@@ -0,0 +1,7 @@
+# user_input = input("enter a sentence containing Nemo: ")
+
+# find_nemo = user_input.find("Nemo")
+# if find_nemo != -1:
+#     print(find_nemo)
+# else:
+#     print("I can't find Nemo :(")
